92135662e407b5f027462da69a1a0b2b102d35a1
Diogo Laurindo <diogo.laurindo01@gmail.com>
2023-08-27 15:44:41 -0300

    Implementing the training/testing data split method and updating brasileirao.csv taking off ';'

--- src/main/java/com/project/data/CollectData.java
+++ src/main/java/com/project/data/CollectData.java
@@ -1,7 +1,35 @@
 package com.project.data;
 
-import com.opencsv.CSVReader;
+import java.util.HashMap;
+
+import tech.tablesaw.api.Table;
 
 public class CollectData {
+    static String csvName = "brasileirao.csv";
     
+    static Table totalData = Table.read().csv(csvName);
+
+    public static void main(String[] args) {
+        System.out.println(totalData.structure());
+    }
+
+    public static HashMap<String, Table> generateTrainingAndTestData() {
+        Table totalDataCopy = totalData.copy();
+
+        Table[] sampleData = totalDataCopy.sampleSplit(0.1);
+
+        Table testingData = sampleData[0];
+        Table trainingData = sampleData[1];
+
+        HashMap<String, Table> hashMap = new HashMap<String, Table>();
+
+        hashMap.put("trainingData", trainingData);
+        hashMap.put("testingData", testingData);
+
+        return hashMap;
+    }
+
+    public static Table getAllData() {
+        return totalData;
+    }
 }

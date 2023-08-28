340b021a3995fa8a5591a3d8410eab3c4c67dc97
Diogo Laurindo <diogo.laurindo01@gmail.com>
2023-08-28 12:41:43 -0300

    Implementing the Table->Matrix function

--- src/main/java/com/project/data/ProcessData.java
+++ src/main/java/com/project/data/ProcessData.java
@@ -1,5 +1,41 @@
 package com.project.data;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
+import tech.tablesaw.api.Row;
+import tech.tablesaw.api.Table;
+
 public class ProcessData {
-    
+    public static void main(String[] args) {
+        HashMap<String, Table> hashMap = CollectData.generateTrainingAndTestData();
+
+        // Only for testing data integrity
+        // Use System.out.println(Arrays.deepToString(nameOfMatrix)) to display all data in form of matrix
+        double[][] trainingDataInputMatrix = generateMatrix(hashMap.get("trainingDataInput"));
+        double[][] trainingDataOutputMatrix = generateMatrix(hashMap.get("trainingDataOutput"));
+        double[][] testingDataInputMatrix = generateMatrix(hashMap.get("testingDataInput"));
+        double[][] testingDataOutputMatrix = generateMatrix(hashMap.get("testingDataOutput"));
+    }
+
+    public static double[][] generateMatrix(Table table) {
+        int numberOfColumns = table.columnCount() - 1;
+        int numberOfRows = table.rowCount();
+
+        double[][] matrix = new double[numberOfRows][numberOfColumns];
+
+        for (int rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+            Row row = table.row(rowIndex);
+
+            for (int rowColumn = 1; rowColumn <= numberOfColumns; rowColumn++) {
+                try {
+                    matrix[rowIndex][rowColumn-1] = row.getInt(rowColumn);
+                } catch (IllegalArgumentException e) {
+                    matrix[rowIndex][rowColumn-1] = row.getTime(rowColumn).getHour();
+                }
+            }
+        }
+
+        return matrix;
+    } 
 }

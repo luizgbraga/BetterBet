fe38907dcc66e38e143e95f6eb164cb2b51a100a
Alexandre <paiva240@gmail.com>
2023-09-02 14:25:50 -0300

    button functionality

--- src/main/java/com/project/ui/InputForm.java
+++ src/main/java/com/project/ui/InputForm.java
@@ -17,12 +17,12 @@ public class InputForm extends javax.swing.JFrame {
 
         txtTitle = new javax.swing.JLabel();
         txtSubTitle = new javax.swing.JLabel();
-        txtMandante = new javax.swing.JLabel();
-        cmbTimeMandante = new javax.swing.JComboBox<>();
-        TxtVisitante = new javax.swing.JLabel();
-        cmbTimeVisitante = new javax.swing.JComboBox<>();
-        txtHora = new javax.swing.JLabel();
-        cmbHora = new javax.swing.JComboBox<>();
+        txtHomeClubName = new javax.swing.JLabel();
+        cmbHomeClubName = new javax.swing.JComboBox<>();
+        txtVisitorClubName = new javax.swing.JLabel();
+        cmbVisitorClubName = new javax.swing.JComboBox<>();
+        txtMatchTime = new javax.swing.JLabel();
+        cmbMatchTime = new javax.swing.JComboBox<>();
         txtArriscar = new javax.swing.JLabel();
         sliderRisk = new javax.swing.JSlider();
         btnEnviar = new javax.swing.JButton();
@@ -37,37 +37,37 @@ public class InputForm extends javax.swing.JFrame {
         txtSubTitle.setForeground(new java.awt.Color(51, 102, 255));
         txtSubTitle.setText("a melhor plataforma de apostas");
 
-        txtMandante.setFont(new java.awt.Font("Ubuntu", 0, 24)); // NOI18N
-        txtMandante.setText("Time mandante");
+        txtHomeClubName.setFont(new java.awt.Font("Ubuntu", 0, 24)); // NOI18N
+        txtHomeClubName.setText("Time mandante");
 
-        cmbTimeMandante.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
-        cmbTimeMandante.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "America-RN", "Athletico-PR", "Atletico-MG", "Avai", "Bahia", "Botafogo-RJ", "Brasiliense", "Corinthians", "Coritiba", "Criciuma", "Cruzeiro", "Figueirense", "Flamengo", "Fluminense", "Fortaleza", "Goias", "Gremio", "Guarani", "Internacional", "Ipatinga", "Juventude", "Nautico", "Palmeiras", "Parana", "Paysandu", "Ponte Preta", "Portuguesa", "Santa Cruz", "Santos", "Sao Caetano", "Sao Paulo", "Sport", "Vasco", "Vitoria" }));
-        cmbTimeMandante.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        cmbTimeMandante.addActionListener(new java.awt.event.ActionListener() {
+        cmbHomeClubName.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
+        cmbHomeClubName.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "America-RN", "Athletico-PR", "Atletico-MG", "Avai", "Bahia", "Botafogo-RJ", "Brasiliense", "Corinthians", "Coritiba", "Criciuma", "Cruzeiro", "Figueirense", "Flamengo", "Fluminense", "Fortaleza", "Goias", "Gremio", "Guarani", "Internacional", "Ipatinga", "Juventude", "Nautico", "Palmeiras", "Parana", "Paysandu", "Ponte Preta", "Portuguesa", "Santa Cruz", "Santos", "Sao Caetano", "Sao Paulo", "Sport", "Vasco", "Vitoria" }));
+        cmbHomeClubName.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        cmbHomeClubName.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cmbTimeMandanteActionPerformed(evt);
+                cmbHomeClubNameActionPerformed(evt);
             }
         });
 
-        TxtVisitante.setFont(new java.awt.Font("Ubuntu", 0, 24)); // NOI18N
-        TxtVisitante.setText("Time visitante");
+        txtVisitorClubName.setFont(new java.awt.Font("Ubuntu", 0, 24)); // NOI18N
+        txtVisitorClubName.setText("Time visitante");
 
-        cmbTimeVisitante.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
-        cmbTimeVisitante.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "America-RN", "Athletico-PR", "Atletico-MG", "Avai", "Bahia", "Botafogo-RJ", "Brasiliense", "Corinthians", "Coritiba", "Criciuma", "Cruzeiro", "Figueirense", "Flamengo", "Fluminense", "Fortaleza", "Goias", "Gremio", "Guarani", "Internacional", "Ipatinga", "Juventude", "Nautico", "Palmeiras", "Parana", "Paysandu", "Ponte Preta", "Portuguesa", "Santa Cruz", "Santos", "Sao Caetano", "Sao Paulo", "Sport", "Vasco", "Vitoria" }));
-        cmbTimeVisitante.addActionListener(new java.awt.event.ActionListener() {
+        cmbVisitorClubName.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
+        cmbVisitorClubName.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "America-RN", "Athletico-PR", "Atletico-MG", "Avai", "Bahia", "Botafogo-RJ", "Brasiliense", "Corinthians", "Coritiba", "Criciuma", "Cruzeiro", "Figueirense", "Flamengo", "Fluminense", "Fortaleza", "Goias", "Gremio", "Guarani", "Internacional", "Ipatinga", "Juventude", "Nautico", "Palmeiras", "Parana", "Paysandu", "Ponte Preta", "Portuguesa", "Santa Cruz", "Santos", "Sao Caetano", "Sao Paulo", "Sport", "Vasco", "Vitoria" }));
+        cmbVisitorClubName.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cmbTimeVisitanteActionPerformed(evt);
+                cmbVisitorClubNameActionPerformed(evt);
             }
         });
 
-        txtHora.setFont(new java.awt.Font("Ubuntu", 0, 24)); // NOI18N
-        txtHora.setText("Hora");
+        txtMatchTime.setFont(new java.awt.Font("Ubuntu", 0, 24)); // NOI18N
+        txtMatchTime.setText("Hora");
 
-        cmbHora.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
-        cmbHora.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30" }));
-        cmbHora.addActionListener(new java.awt.event.ActionListener() {
+        cmbMatchTime.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
+        cmbMatchTime.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30" }));
+        cmbMatchTime.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cmbHoraActionPerformed(evt);
+                cmbMatchTimeActionPerformed(evt);
             }
         });
 
@@ -100,22 +100,21 @@ public class InputForm extends javax.swing.JFrame {
                         .addGap(109, 109, 109))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txtMandante)
-                            .addComponent(cmbTimeMandante, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(TxtVisitante)
-                            .addComponent(cmbTimeVisitante, 0, 368, Short.MAX_VALUE)
-                            .addComponent(txtHora)
-                            .addComponent(cmbHora, 0, 368, Short.MAX_VALUE)
+                            .addComponent(txtHomeClubName)
+                            .addComponent(cmbHomeClubName, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtVisitorClubName)
+                            .addComponent(cmbVisitorClubName, 0, 368, Short.MAX_VALUE)
+                            .addComponent(txtMatchTime)
+                            .addComponent(cmbMatchTime, 0, 368, Short.MAX_VALUE)
                             .addComponent(txtArriscar)
                             .addComponent(sliderRisk, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(34, 34, 34))))
             .addGroup(layout.createSequentialGroup()
                 .addGap(52, 52, 52)
                 .addComponent(txtSubTitle))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(btnEnviar)
-                .addGap(169, 169, 169))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(179, 179, 179)
+                .addComponent(btnEnviar))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -125,47 +124,56 @@ public class InputForm extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtSubTitle)
                 .addGap(38, 38, 38)
-                .addComponent(txtMandante)
+                .addComponent(txtHomeClubName)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(cmbTimeMandante, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(cmbHomeClubName, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(TxtVisitante)
+                .addComponent(txtVisitorClubName)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(cmbTimeVisitante, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(cmbVisitorClubName, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(txtHora)
+                .addComponent(txtMatchTime)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(cmbHora, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(cmbMatchTime, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(txtArriscar)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(sliderRisk, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addComponent(btnEnviar, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(23, Short.MAX_VALUE))
+                .addContainerGap(17, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>                        
 
-    private void cmbTimeMandanteActionPerformed(java.awt.event.ActionEvent evt) {                                                
+    private void cmbHomeClubNameActionPerformed(java.awt.event.ActionEvent evt) {                                                
         // TODO add your handling code here:
-        String timeMandanteSelecionado = cmbTimeMandante.getSelectedItem().toString();
+        String timeMandanteSelecionado = cmbHomeClubName.getSelectedItem().toString();
     }                                               
 
     private void btnEnviarActionPerformed(java.awt.event.ActionEvent evt) {                                          
         // TODO add your handling code here:
+        String timeMandanteSelecionado = cmbHomeClubName.getSelectedItem().toString();
+        String timeVisitanteSelecionado = cmbVisitorClubName.getSelectedItem().toString();
+        String horarioSelecionado = cmbMatchTime.getSelectedItem().toString();
+        int riscoSelecionado = sliderRisk.getValue();
+
         // Limpar os campos
+        cmbHomeClubName.setSelectedIndex(0);
+        cmbVisitorClubName.setSelectedIndex(0);
+        cmbMatchTime.setSelectedIndex(0);
+        sliderRisk.setValue(85);
         
     }                                         
 
-    private void cmbTimeVisitanteActionPerformed(java.awt.event.ActionEvent evt) {                                                 
+    private void cmbVisitorClubNameActionPerformed(java.awt.event.ActionEvent evt) {                                                   
         // TODO add your handling code here:
-    }                                                
+    }                                                  
 
-    private void cmbHoraActionPerformed(java.awt.event.ActionEvent evt) {                                        
+    private void cmbMatchTimeActionPerformed(java.awt.event.ActionEvent evt) {                                             
         // TODO add your handling code here:
-    }                                       
+    }                                            
 
     /**
      * @param args the command line arguments
@@ -204,17 +212,16 @@ public class InputForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify                     
-    private javax.swing.JLabel TxtVisitante;
     private javax.swing.JButton btnEnviar;
-    private javax.swing.JComboBox<String> cmbHora;
-    private javax.swing.JComboBox<String> cmbTimeMandante;
-    private javax.swing.JComboBox<String> cmbTimeVisitante;
+    private javax.swing.JComboBox<String> cmbHomeClubName;
+    private javax.swing.JComboBox<String> cmbMatchTime;
+    private javax.swing.JComboBox<String> cmbVisitorClubName;
     private javax.swing.JSlider sliderRisk;
     private javax.swing.JLabel txtArriscar;
-    private javax.swing.JLabel txtHora;
-    private javax.swing.JLabel txtMandante;
+    private javax.swing.JLabel txtHomeClubName;
+    private javax.swing.JLabel txtMatchTime;
     private javax.swing.JLabel txtSubTitle;
     private javax.swing.JLabel txtTitle;
+    private javax.swing.JLabel txtVisitorClubName;
     // End of variables declaration                   
 }
-
